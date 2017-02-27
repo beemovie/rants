bb04169ed11b973674f411db7ac362bccd3e6d65
Rants Authors <rants@donotcontact>
2017-02-27 15:40:11 -0800

    all: Create stub GAE app with index, api

---
+++ server.go
@@ -0,0 +1,68 @@
+// Copyright (C) 2017 Anonymous
+// RWTXliExJCquO54R+qP94i4V+X8bQegE6L9EjhKIH23ePweJG8u7dqDK
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version. See COPYING for the full text of the License.
+
+package rants
+
+import (
+	"encoding/xml"
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+type State struct {
+	License template.HTML
+	ctx     context.Context
+	tmpl    *template.Template
+}
+
+func newState() *State {
+	l, err := ioutil.ReadFile("templates/license-snippet.txt")
+	if err != nil {
+		panic(err)
+	}
+	license := template.HTML(string(l))
+
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	return &State{
+		License: license,
+		tmpl:    tmpl,
+	}
+}
+
+func (c *State) error(err error) {
+	log.Errorf(c.ctx, "%v", err)
+}
+
+func (c *State) index(w http.ResponseWriter, r *http.Request) {
+	c.ctx = appengine.NewContext(r)
+	w.Header().Set("Content-Type", "text/html")
+	if err := c.tmpl.Execute(w, c); err != nil {
+		c.error(err)
+	}
+}
+
+func (c *State) api(w http.ResponseWriter, r *http.Request) {
+	c.ctx = appengine.NewContext(r)
+	w.Header().Set("Content-Type", "text/xml")
+	io.WriteString(w, xml.Header)
+}
+
+func init() {
+	c := newState()
+	http.HandleFunc("/", c.index)
+	http.HandleFunc("/v1/", c.api)
+}
